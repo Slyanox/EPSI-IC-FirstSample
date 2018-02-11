d12b597deff09ff2ea3bf3083beb4cd93b07fd72
Valentin Mesny <valentin.mesny@outlook.com>
2018-02-11 17:00:46 +0100

    Ajoutez des fichiers projet.

---
+++ EPSI-IC-FirstSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EPSI_IC_FirstSample.MvcApplication" Language="C#" %>
---
+++ EPSI-IC-FirstSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EPSI_IC_FirstSample.Startup))]
+namespace EPSI_IC_FirstSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
