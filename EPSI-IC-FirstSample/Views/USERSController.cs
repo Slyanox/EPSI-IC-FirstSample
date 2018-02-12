e4e74bdece8b7ece219e0e959bb5ec8322359c4c
Valentin MESNY <valentin.mesny@outlook.com>
2018-02-12 08:56:34 +0100

    Mise en place de la BDD

---
+++ EPSI-IC-FirstSample/Views/USERSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EPSI_IC_FirstSample.Models;
+
+namespace EPSI_IC_FirstSample.Views
+{
+    public class USERSController : Controller
+    {
+        private BDDIntégrationEntities db = new BDDIntégrationEntities();
+
+        // GET: USERS
+        public ActionResult Index()
+        {
+            return View(db.USERS.ToList());
+        }
+
+        // GET: USERS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERS uSERS = db.USERS.Find(id);
+            if (uSERS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSERS);
+        }
+
+        // GET: USERS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: USERS/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_USER,NAME,FIRSTNAME,AGE,EMAIL")] USERS uSERS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USERS.Add(uSERS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uSERS);
+        }
+
+        // GET: USERS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERS uSERS = db.USERS.Find(id);
+            if (uSERS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSERS);
+        }
+
+        // POST: USERS/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_USER,NAME,FIRSTNAME,AGE,EMAIL")] USERS uSERS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSERS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(uSERS);
+        }
+
+        // GET: USERS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERS uSERS = db.USERS.Find(id);
+            if (uSERS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSERS);
+        }
+
+        // POST: USERS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            USERS uSERS = db.USERS.Find(id);
+            db.USERS.Remove(uSERS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
